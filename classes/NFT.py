c0a8aaee748ac9fca06f2c377fa32451e2857b6c
David Thomas <dave_thomas@enoughwebapp.com>
2023-08-10 13:10:32 -0500

    started web3 utils

---
+++ classes/NFT.py
@@ -0,0 +1,11 @@
+""" Establishing the NFT class """
+
+class NFT:
+    """ NFT class for NFTs """
+    def __init__(self, name, description, image, price, owner):
+        self.name = name
+        self.description = description
+        self.image = image
+        self.price = price
+        self.owner = owner
+
--- utils/web3_utils.py
+++ utils/web3_utils.py
@@ -1,17 +1,9 @@
 """ Utilities related to interacting with the Ethereum blockchain.
 This will utilize the web3.py library and reference a Vyper smart contract
 named VoiceprintNFT"""
-from web3 import Web3
-from eth_account.messages import encode_defunct
-from eth_account import Account, messages
-from eth_utils import keccak
-from eth_typing import Address
-import requests
-import streamlit as st
-import json
 import os
-from dotenv import load_dotenv
-load_dotenv()
+import requests
+
 
 # Load the environment variables
 crypto_key = os.getenv("CRYPTO_API_KEY")
@@ -28,8 +20,5 @@ def convert_to_ether(usd_amount: float) -> float:
 
 def convert_to_wei(amount_ether: float) -> int:
     """ Convert a float amount of Ether to Wei."""
-    return Web3.toWei(amount_ether, 'ether')
+    return amount_ether * 10**18
 
-def mint_nft(recipient: Address, voicePrintReference: str, _salePrice: int):
-    """ Mint a new NFT and transfer it to the recipient address."""
-    return {"recipient": recipient, "voicePrintReference": voicePrintReference, "salePrice": _salePrice}
