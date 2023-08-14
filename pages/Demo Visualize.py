3a08632824fba887db85623575bf6f1373389a47
David Thomas <dave_thomas@enoughwebapp.com>
2023-08-13 22:14:09 -0500

    testing new plots

--- pages/Demo Visualize.py
+++ pages/Demo Visualize.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import plotly.express as px
+from speechbrain.processing.features import STFT, ISTFT
+import torch
+from speechbrain.dataio.dataio import read_audio
 from utils.audio_processing import (
     load_audio, plot_waveform, get_spectrogram, plot_spectrogram,
     generate_mel_spectrogram, get_mfcc, get_lfcc, get_pitch, plot_pitch,
@@ -96,5 +99,81 @@ def plot_feature(waveform, sr, feature_option, audio_file, label):
         col2.audio(audio_file, format='audio/wav')
 
 # Run this page
-if __name__ == "__main__":
+#if __name__ == "__main__":
     demo_visualize_page()
+
+
+def test_plots():
+    """ Test plotting functions / reconstructing audio """
+    avicii_signal = read_audio('./audio_samples/avicii1.wav').unsqueeze(0)
+    compute_STFT = STFT(sample_rate=16000, n_fft=400,
+                        hop_length=10, win_length=25)
+    avicii_STFT = compute_STFT(avicii_signal)
+    combs_signal = read_audio('./audio_samples/combs1.wav').unsqueeze(0)
+    combs_STFT = compute_STFT(combs_signal)
+    st.write(combs_STFT.shape)    
+    compute_ISTFT = ISTFT(sample_rate=16000,win_length=25, hop_length=10)
+    avicii_recon = compute_ISTFT(avicii_STFT).squeeze()
+    combs_recon = compute_ISTFT(combs_STFT)
+
+
+    # Create a dataframe from the STFT
+    avicii_df = pd.DataFrame(avicii_recon)
+    # Drop the last column
+    st.write(avicii_df)
+    combs_df = pd.DataFrame(combs_recon)
+    # Drop the last column
+    st.write(combs_df)
+    st.stop()
+
+    # Concatenate the two dataframes
+    df = pd.concat([avicii_df, combs_df], axis=1)
+
+    # Drop the null values
+    df = df.dropna()
+    st.write(df)
+    st.stop()
+
+    # Perform PCA
+    pca = PCA(n_components=3)
+    pca_df = pca.fit_transform(df)
+
+    # Perform KMeans clustering
+    kmeans = KMeans(n_clusters=4, random_state=42)
+    kmeans.fit(pca_df)
+    clusters = kmeans.predict(pca_df)
+
+    # Create a DataFrame
+    cluster_df = pd.DataFrame(pca_df, columns=['PC1', 'PC2', 'PC3'])
+    cluster_df['cluster'] = clusters
+
+    # Save to CSV file
+    cluster_df.to_csv('./resources/cluster_results.csv', index=False)
+    df.to_csv('./resources/avicii_combs.csv', index=False)
+
+    # 3D scatter plot
+    fig = px.scatter_3d(
+        cluster_df,
+        x='PC1',
+        y='PC2',
+        z='PC3',
+        color='cluster',
+        color_continuous_scale='viridis',
+        title='KMeans Clustering of Audio Features'
+    )
+
+    fig.update_layout(
+        scene=dict(
+            xaxis=dict(title='PC1'),
+            yaxis=dict(title='PC2'),
+            zaxis=dict(title='PC3')
+        )
+    )
+    st.plotly_chart(fig)
+
+    
+
+
+test_plots()
+    
+  
