ac0aa38c34e6105b344ef35e43e3af0d8b8517e6
David Thomas <dave_thomas@enoughwebapp.com>
2023-09-02 15:12:45 -0500

    deleted old co-writer page

--- pages/Co-writer.py
+++ pages/Co-writer.py
@@ -1,166 +1,127 @@
-""" UI for the user to be able to interact with the artist Luke Combs """
-import uuid
+""" This file contains the code for the co-writer page.  This page allows
+the user to chat with Luke Combs and receive guidance on their song writing.
+This can be in the form of text or audio."""
+import os
 import numpy as np
+import openai
+import pinecone
 import streamlit as st
-from streamlit_chat import message
-from streamlit_extras.switch_page_button import switch_page
-from PIL import Image
-from utils.chat_utils import add_message
-from utils.model_utils import get_luke_response, get_audio_sample, get_inputs_from_llm
+from utils.model_utils2 import get_vectorstore, get_context, get_inputs_from_llm, get_audio_sample
 
-# Define the page config
-st.set_page_config(page_title="Luke Combs Chat", initial_sidebar_state="collapsed")
 
-if "chat_page" not in st.session_state:
-    st.session_state.chat_page = "chat_home"
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
-if "user_name" not in st.session_state:
-    st.session_state.user_name = ""
-if "context" not in st.session_state:
-    st.session_state.context = []
-if "output" not in st.session_state:
-    st.session_state.output = None
-if "prompt" not in st.session_state:
-    st.session_state.prompt = ""
 
-
-def chat_home():
-    """ Home page for chat with training pipeline image """
-    # Insert the training pipeline image
-    training_pipeline_image = Image.open("images/training_pipeline.png")
-    st.image(training_pipeline_image, use_column_width=True)
-
-    st.text("")
-    st.markdown("**Above we can see the basic training pipeline for creating\
-                boutique LLMs that are tailored to the specific artist and can\
-                be used downtstream for a variety of applications.**")
-    
-    st.markdown("**For a simple demo, we have created a chatbot that takes\
-                on the persona of the artist Luke Combs.  You can engage\
-                with Luke to get his thoughts on your songwriting.  Obviously,\
-                the uniquely trained model would be much more robust, but\
-                this demo should give you a sense of the potential.**")
-    # Create the button to start the chat
-    start_chat_button = st.button("Start Co-writing with Luke", type="primary",
-                                  use_container_width=True)
-    if start_chat_button:
-        # Switch to the chat intro page
-        st.session_state.chat_page = "chat_intro"
-        st.experimental_rerun()
-
-def chat_intro():
-    """ The home page for the artist chat interactions """
-    st.session_state.chat_history = []
-    st.markdown("##### Welcome to Co-writer!  Luke Combs\
-                is here to help guide you and encourage you\
-                as you write your next hit song!")
+if not st.session_state.chat_history:
+    st.success("**Welcome to co-writer!  There are two options for the chat:**\
+                The first option is a standard text chat back and forth where\
+                Luke can help you brainstorm ideas for your song.  The second\
+                allows you to request that Luke help you compose an audio clip\
+                based on your chat history and ideas generated.  You may switch\
+                back and forth at any time to test them out!  Check out the sidebar\
+                to make your selection.")
     st.markdown("---")
-    # Display an initial greeting from the artist
-    initial_greeting = "Hey there!  I'm Luke Combs, and I'm\
-                        excited to co-write with you!  Why don't you tell me\
-                        about the song you're working on?"
-    message(initial_greeting, avatar_style="initials", seed="LC")
-   
-    # Add the initial greeting to the chat history
-    add_message("ai", initial_greeting)
-    # Get the user's question
-    user_question = st.text_area("Your message for Luke", height=100)
-    # Create a button to send the question to the artist
-    send_question_button = st.button("Send Your Message to Luke", type="primary",
-                                     use_container_width=True)
-    if send_question_button:
+    with st.chat_message("assistant", avatar="🎸"):
+        st.markdown("Hello, friend.  I'm excited to get our co-writing\
+                    session started!  Why don't you tell me a little bit\
+                    about the song you are working on?")
+
+for message in st.session_state.chat_history:
+    if message["role"] == "user":
+        with st.chat_message("user"):
+            st.markdown(message["content"])
+    else:
+        with st.chat_message("assistant", avatar="🎸"):
+            st.markdown(message["content"])
+
+response_type = st.sidebar.radio("Choose your response type", ("Standard Chat", "Musical Chat"))
+
+def get_text_response():
+    """ Get a response from Luke Combs in the form of text."""
+    if prompt := st.chat_input("Your message for Luke:"):
         with st.spinner("Luke is writing..."):
-            # Get the artist's response
-            get_luke_response(user_question)
-            # Set the session state to display the chat history
-            st.session_state.chat_page = "display_chat"
-            st.experimental_rerun()    
-    
+            st.session_state.chat_history.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            with st.chat_message("assistant", avatar = "🎸"):
+                pinecone.init(api_key = os.getenv("PINECONE_KEY"),
+                environment=os.getenv("PINECONE_ENV")) # Initialize pinecone
+                vectorstore = get_vectorstore(index_name = 'combs-data')
+                context = get_context(vectorstore, prompt)
+                context_dict = [{"Song Name" : context.page_content, "lyrics" : context.metadata} for context in context]
+                context = context_dict
+                st.session_state.context = context
+                messages = [
+                    {
+                        "role": "system", "content": f"""You are Luke Combs, the famous
+                        country music singer, helping a fan out in a "co-writing" session
+                        where you are giving them advice based on your own style to help 
+                        them write songs.  You have context {context} pulled from your song
+                        lyrics to help you relate to the user's question {prompt}.  Feel free
+                        to mention a specific song or lyrics of yours when guiding the users along.
+                        Your chat history so far is {st.session_state.chat_history}.  This will
+                        be a back and forth chat, so make sure to leave your responses open-ended."""
+                    },
+                    {
+                        "role": "user", "content": f"""Please answer my {prompt} about 
+                        song writing."""
+                    },
+                ]
+                message_placeholder = st.empty()
+                full_response = ""
+                models = ["gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0613", "gpt-3.5-turbo"] # Set list of models to iterate through
+                for model in models:
+                    try:
+                        for response in openai.ChatCompletion.create(
+                            model=model,
+                            messages = messages,
+                            max_tokens=250,
+                            frequency_penalty=0.5,
+                            presence_penalty=0.5,
+                            temperature=1,
+                            n=1,
+                            stream=True
+                        ):
+                            full_response += response.choices[0].delta.get("content", "")
+                            message_placeholder.markdown(full_response + "▌")
+                            if response.choices[0].delta.get("stop"):
+                                break
+                        break 
+                    except Exception as e:
+                        print(e)
+                        continue
+                
+            message_placeholder.markdown(full_response)
+        st.session_state.chat_history.append({"role": "assistant", "content": full_response})
+  
 
-def display_chat():
-    """ Display the ongoing chat """
-    st.markdown(f"**Generated Prompts:** {st.session_state.prompt}")
-    st.warning("""**The way this is currently set up, the user
-               can chat with Luke and receive text / lyric guidance,
-               etc. by clicking the "Send Message to Luke" button.
-               The conversation will continue as long as the user
-               likes.  If at any point the user would like to generate
-               an audio clip based on the chat, they can click the
-               "Request Audio Sample" button.**""")
-    st.text("")
-    st.text("")
-    # Get the user's next question
-    user_question = st.text_area("Your message for Luke", height=100)
-    # Create a button to send the question to the artist
-    send_question_button = st.button("Send Message to Luke", type="primary", use_container_width=True)
-    if send_question_button:
-        with st.spinner("Luke is typing..."):
-            # Get the artist's response
-            get_luke_response(user_question)
-            # Set the session state to display the chat history
-            st.session_state.chat_page = "display_chat"
-            user_question = ""
-            st.experimental_rerun()
-     # Create a button to request an audio sample
-    request_audio_button = st.button("Request Audio Sample", type="primary",
-                                     use_container_width=True)
-    if request_audio_button:
-        with st.spinner("Luke is composing... This will take\
-                        a minute"):
-            st.session_state.output = None
-            add_message("user", user_question)
-            # Get the artist's response
-            prompt = get_inputs_from_llm()
-            output = get_audio_sample(prompt)
-            st.session_state.output = output
-            # Set the session state to display the chat history
-            st.session_state.chat_page = "display_chat"
-            st.experimental_rerun()
-    st.markdown("---")
-    if st.session_state.output:
-        st.markdown("**Current Audio Sample**")
-        # Convert the audio sample to a streamlit audio object
-        st.audio(np.array(st.session_state.output), format='audio/wav', start_time=0, sample_rate=22050)
-    chat_container = st.container()
-    with chat_container:
-        # Display the chat history
-        for chat_message in st.session_state.chat_history[-2:]:
-            # If the role is "ai", display the message on the left
-            if chat_message['role'] == "assistant":
-                message(chat_message['content'], avatar_style="initials", seed="LC",
-                        key = f'{uuid.uuid4()}')
-            # If the role is "user", display the message on the right
-            elif chat_message['role'] == "user":
-                message(chat_message['content'], avatar_style="miniavs", seed="Socks",
-                        is_user=True, key=f'{uuid.uuid4()}')
+def get_music_response():
+    """ Get a response from Luke Combs in the form of an audio clip.
+    We generate the clip by calling ChatGPT to create inputs based on 
+    the chat history that are then fed into the MusicGen model to create
+    an audio clip.  The audio clip is then returned to the user."""
+    if prompt := st.chat_input("Your message for Luke:"):
+        with st.spinner("Luke is composing... This will take a minute"):
+            st.session_state.chat_history.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            with st.chat_message("assistant", avatar = "🎸"):
+                message_placeholder = st.empty()
+                full_response = ""
+                inputs = get_inputs_from_llm()
+                output = get_audio_sample(inputs)
+                message_placeholder.audio(np.array(output), sample_rate=22050)
+            st.session_state.chat_history.append({"role": "assistant", "content": full_response})
 
+# Create a button to reset the chat history
+reset_button = st.sidebar.button("Reset Chat History", type="primary", use_container_width=True)
+if reset_button:
+    st.session_state.chat_history = []
+    st.experimental_rerun()
 
-    st.markdown("---")
-    # Create a button to start a new chat
-    new_chat_button = st.button("New Chat", type="primary", use_container_width=True)
-    if new_chat_button:
-        # Clear the chat history
-        st.session_state.chat_history = []
-        # Reset the output
-        st.session_state.output = None
-        # Switch to the chat home page
-        st.session_state.chat_page = "chat_home"
-        st.experimental_rerun()
-    
-    # Create a button to go back to the demo home page
-    home_button = st.button("Home", type="primary", use_container_width=True)
-    if home_button:
-        # Clear the chat history
-        st.session_state.chat_history = []
-        # Switch to the chat home page
-        st.session_state.chat_page = "chat_home"
-        switch_page("Demo Visualize")
-
-# Set the flow of the page
-if st.session_state.chat_page == "chat_home":
-    chat_home()
-elif st.session_state.chat_page == "chat_intro":
-    chat_intro()
-elif st.session_state.chat_page == "display_chat":
-    display_chat()
+if response_type == "Standard Chat":
+    get_text_response()
+else:
+    get_music_response()
+           
+                
